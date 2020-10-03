3b421a41ed6546254fc151de3465f1da250973a4
Olukowajo Funmilola <estherolukowajo@yahoo.com>
2020-10-03 16:05:09 +0100

    modified agecal.js

--- agecal.js
+++ agecal.js
@@ -1,14 +1,16 @@
 const page1 = document.querySelector("#home");
 const page2 = document.querySelector("#form");
-const page3 = document.getElementById("result");
+const page3 = document.querySelector(".resultpage");
 const startButton = document.getElementById("startButton");
 const submitButton = document.querySelector(".button2");
 const resetButton = document.getElementById("resetButton");
-const displayName = document.querySelector(".pnmae");
+const displayName = document.querySelector(".pname");
 const displayAge = document.querySelector('.pyears');
 
 
 startButton.addEventListener("click",function(){
+    
+    
     page2.classList.remove("hidden");
     page1.classList.add("hidden");
 })
@@ -16,7 +18,9 @@ startButton.addEventListener("click",function(){
 submitButton.addEventListener("click",function(e){
     e.preventDefault();
 
-    console.log('hello');
+   
+
+   // console.log('hello');
     let name = document.querySelector(".input11").value;
     let age = document.querySelector(".input2").value;
     console.log(name,age);
@@ -28,10 +32,13 @@ submitButton.addEventListener("click",function(e){
         return;
     }
     else {
-        displayName.textContent += name;
+        displayName.textContent = `Your name is `+ name;
         ageInDays = age*365;
-        displayAge.textContent += age;
-        displayDays.textContent +=ageInDays;
+        displayAge.textContent = `You have lived ${ageInDays} days on earth`;
+      
+        
+        
+        //displayDays.textContent +=ageInDays;
 
     }
 
@@ -40,6 +47,12 @@ submitButton.addEventListener("click",function(e){
 })
 
 resetButton.addEventListener("click", function(){
+
+    // console.log('hello');
+ document.querySelector(".input11").value = '';
+ document.querySelector(".input2").value = ''; 
+    
+   
     page1.classList.remove("hidden");
     page3.classList.add("hidden");
 });
